f83a947ddfa1254a19c3b2586a1942bc15df007a
joulianos02 <78118644+joulianos02@users.noreply.github.com>
2021-04-06 19:34:30 -0400

    Nouveau projet et suppression de l'ancien

---
+++ Exam_AQL/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Projet Final Assurance Qualitée Logiciel
+ *Développé par : Julien Lemieux et Mathieu Labre  
+ *Date : 2021-04-06 
+ */
+using System;
+
+namespace Exam_AQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projet final!");
+        }
+    }
+}
