709a0d8f6f4d261e540d798001c2e48cd1b1c21b
joulianos02 <78118644+joulianos02@users.noreply.github.com>
2021-04-15 12:54:16 -0400

    Delete FileInfoExtensions Class

--- Exam_AQL/Note.cs
+++ Exam_AQL/Note.cs
@@ -57,15 +57,6 @@ namespace Exam_AQL
          
      
         }   
-        }
-
     }
-       public static class FileInfoExtensions
-        {
-            public static void Rename(this FileInfo fileInfo, string newName)
-            {
-                fileInfo.MoveTo(Path.Combine(fileInfo.Directory.FullName, newName));
-            }
-        }
 
 } 
